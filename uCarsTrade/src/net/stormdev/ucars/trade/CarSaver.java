e3e6df867972cfe156853b14ff94eec0a55fef6a
storm345 <storm345dev@gmail.com>
2014-01-02 16:16:51 +0000

    Add efficient methods to carSaver

--- uCarsTrade/src/net/stormdev/ucars/trade/CarSaver.java
+++ uCarsTrade/src/net/stormdev/ucars/trade/CarSaver.java
@@ -9,13 +9,75 @@ import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.UUID;
 
+import org.bukkit.scheduler.BukkitRunnable;
+
 import net.stormdev.ucars.utils.Car;
 
 public class CarSaver {
 	public HashMap<UUID, Car> cars = new HashMap<UUID, Car>();
+	public HashMap<UUID, Car> cache = new HashMap<UUID, Car>();
 	File saveFile = null;
 	public CarSaver(File saveFile){
 		this.saveFile = saveFile;
+	}
+	public synchronized Boolean isACar(UUID carId){
+		if(cache.containsKey(carId)){
+			return true;
+		}
+		Boolean b = cars.containsKey(carId);
+		if(b){
+			cache.put(carId, cars.get(carId));
+			cacheSize();
+		}
+		return b;
+	}
+	public synchronized Car getCar(UUID carId){
+		Car c = cache.get(carId);
+		if(c != null){
+			return c;
+		}
+		return cars.get(carId);
+	}
+	public synchronized void setCar(UUID carId, Car car){
+		if(!car.isPlaced){
+			cache.remove(carId);
+		}
+		else{
+			cache.put(carId, car);
+		}
+		cars.put(carId, car);
+		cacheSize();
+		asyncSave();
+	}
+	public synchronized void removeCar(UUID carId){
+		cache.remove(carId);
+		cars.remove(cars);
+		asyncSave();
+	}
+	public void updateCar(UUID old, Car current){
+		removeCar(old);
+		setCar(current.id, current);
+		asyncSave();
+	}
+	public synchronized void cacheSize(){
+		while(cache.size() > 20){ //Maximum car cache
+			cache.remove(cache.keySet().toArray()[0]); //Clear it back to size
+		}
+		return;
+	}
+	public synchronized void removeFromCache(UUID carId){
+		cache.remove(carId);
+	}
+	public void noLongerPlaced(UUID carId){
+		removeFromCache(carId);
+	}
+	public void asyncSave(){
+		main.plugin.getServer().getScheduler().runTaskAsynchronously(main.plugin, new BukkitRunnable(){
+
+			public void run() {
+				save();
+				return;
+			}});
 	}
 	public void load(){
 		this.saveFile.getParentFile().mkdirs();

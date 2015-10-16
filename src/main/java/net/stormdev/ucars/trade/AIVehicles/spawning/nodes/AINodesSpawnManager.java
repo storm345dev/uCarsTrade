aa8e466b7cdb0827a77ef11da25e4c5faea23612
storm345 <eddiebbrown05@gmail.com>
2015-10-16 19:54:13 +0100

    Increase car spawnrate a bit

--- src/main/java/net/stormdev/ucars/trade/AIVehicles/spawning/nodes/AINodesSpawnManager.java
+++ src/main/java/net/stormdev/ucars/trade/AIVehicles/spawning/nodes/AINodesSpawnManager.java
@@ -84,7 +84,7 @@ public class AINodesSpawnManager extends AbstractAISpawnManager {
 						continue;
 					}
 					
-					int chance = 60;
+					int chance = 50;
 					chance -= (activeNodes.size()*0.3d); //Make it more likely to spawn a car; the more nodes there are active
 					if(chance < 2){
 						chance = 2;
@@ -118,7 +118,7 @@ public class AINodesSpawnManager extends AbstractAISpawnManager {
 						continue;
 					}
 					
-					chance = (int) ((nearCount*3.5) + 1);
+					chance = (int) ((nearCount*2.3) + 1);
 					if(!(main.random.nextInt(chance) <= 1)){ //Avoids LOTS of cars spawning where there's LOTS of players
 						continue;
 					}

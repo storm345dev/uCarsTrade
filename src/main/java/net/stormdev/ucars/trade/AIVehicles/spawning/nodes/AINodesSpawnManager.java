43db9ef3ca3ef9274c58a0e4782d3cfcb39fa693
storm345 <eddiebbrown05@gmail.com>
2015-10-16 21:00:44 +0100

    MORE car spawning

--- src/main/java/net/stormdev/ucars/trade/AIVehicles/spawning/nodes/AINodesSpawnManager.java
+++ src/main/java/net/stormdev/ucars/trade/AIVehicles/spawning/nodes/AINodesSpawnManager.java
@@ -84,7 +84,7 @@ public class AINodesSpawnManager extends AbstractAISpawnManager {
 						continue;
 					}
 					
-					int chance = 50;
+					int chance = 45;
 					chance -= (activeNodes.size()*0.4d); //Make it more likely to spawn a car; the more nodes there are active
 					if(chance < 2){
 						chance = 2;

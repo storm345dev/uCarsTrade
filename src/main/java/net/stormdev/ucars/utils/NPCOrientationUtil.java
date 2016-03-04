fe5b1f30b23f526fc6f7a5d3fb5065db0defd716
storm345 <eddiebbrown05@gmail.com>
2016-03-04 18:24:14 +0000

    Fix NPC head rotations in cars

--- src/main/java/net/stormdev/ucars/utils/NPCOrientationUtil.java
+++ src/main/java/net/stormdev/ucars/utils/NPCOrientationUtil.java
@@ -1,15 +1,21 @@
 package net.stormdev.ucars.utils;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Set;
+import java.util.List;
 
 import net.minecraft.server.v1_9_R1.Entity;
 import net.minecraft.server.v1_9_R1.EntityVillager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_9_R1.entity.CraftVillager;
-import org.bukkit.craftbukkit.v1_9_R1.util.UnsafeList;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 
 public class NPCOrientationUtil {
 	/*public static void setPitch(Villager v, float pitch){
@@ -38,6 +44,34 @@ public class NPCOrientationUtil {
 			e.printStackTrace();
 		}
 		
+		ve.h(yaw); //Set head rotation
+		
+		/*ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+		PacketContainer headPos = manager.createPacket(PacketType.Play.Server.ENTITY_HEAD_ROTATION);
+		
+		float ya = yaw;
+		while(ya < 0){
+			ya+=360;
+		}
+		while(ya > 360){
+			ya-=360;
+		}
+		float angle = (yaw*256.0f/360.0f);
+		int a = (int) angle;
+		headPos.getIntegers().write(0, ve.getId());
+		headPos.getBytes().write(0, (byte) a);
+		
+		List<org.bukkit.entity.Entity> near = v.getNearbyEntities(50, 5, 50);
+		for(org.bukkit.entity.Entity e:near){
+			if(e instanceof Player){
+				try {
+					manager.sendServerPacket((Player) e, headPos);
+				} catch (InvocationTargetException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		*/
 		/*try {
 			Class<?> cmr = v.getClass();
 			Method getHandle = cmr.getMethod("getHandle");

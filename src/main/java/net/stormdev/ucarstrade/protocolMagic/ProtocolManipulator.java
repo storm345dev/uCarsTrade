08ea58842f5eea73b1205b5ebe493441eaa47288
storm345 <eddiebbrown05@gmail.com>
2020-08-23 13:02:04 +0100

    Fix index

--- src/main/java/net/stormdev/ucarstrade/protocolMagic/ProtocolManipulator.java
+++ src/main/java/net/stormdev/ucarstrade/protocolMagic/ProtocolManipulator.java
@@ -161,8 +161,8 @@ public class ProtocolManipulator implements Listener {
                     double yawRad5 = (hce.getBukkitYaw()+hce.getFakeBoatRotationOffsetDeg(4))*(Math.PI/180);
                     sendFakeEntitySpawn(player, hce.getFakeBoat4(), hc.getLocation(), yawRad4, pitchRad, 1);
                     sendFakeEntitySpawn(player, hce.getFakeBoat5(), hc.getLocation(), yawRad5, pitchRad, 1);
-                    sendFakeEntitySpawn(player, hce.getFakeArrow4(), hc.getLocation(), yawRad4, pitchRad, 60);
-                    sendFakeEntitySpawn(player, hce.getFakeArrow5(), hc.getLocation(), yawRad5, pitchRad, 60);
+                    sendFakeEntitySpawn(player, hce.getFakeArrow5(), hc.getLocation(), yawRad4, pitchRad, 60);
+                    sendFakeEntitySpawn(player, hce.getFakeArrow6(), hc.getLocation(), yawRad5, pitchRad, 60);
                 }
             }
             hce.setKnowAboutFakeEntities(player,true);
